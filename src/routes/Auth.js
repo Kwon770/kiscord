a1f811f801d7e6b581f3cf002681c33078c76573
Kwon770 <sckwon770@gmail.com>
2020-10-17 02:59:51 +0900

    feat: Add wave animation from auth

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -3,10 +3,15 @@ import styled from "styled-components";
 import AuthForm from "components/AuthForm";
 import { FaDiscord } from "react-icons/fa";
 import wallpaper from "asset/AuthWallpaper.jpg";
+import Wave from "react-wavify";
 
 const Auth = () => {
   return (
     <Holder>
+      <WaveHolder>
+        <Wave fill="#2F3136" options={{ amplitude: 50, points: 4 }} />
+      </WaveHolder>
+      <WaveBackground />
       <LogoHolder>
         <FaDiscord style={{ width: 34, height: 37, color: "white" }} />
         <LogoTitle>KISCORD</LogoTitle>
@@ -46,6 +51,28 @@ const LogoTitle = styled.text`
   font-size: 22px;
   font-weight: 700;
   margin-left: 4px;
+`;
+
+const WaveHolder = styled.div`
+  position: absolute;
+  top: 70px;
+  left: -300px;
+  padding-bottom: 100px;
+  width: 1200px;
+  height: 300px;
+  border-top-right-radius: 150px;
+  border-top-left-radius: 150px;
+  transform: rotate(135deg);
+`;
+
+const WaveBackground = styled.div`
+  position: absolute;
+  top: -500px;
+  left: -200px;
+  width: 700px;
+  height: 1200px;
+  background-color: #2f3136;
+  transform: rotate(45deg);
 `;
 
 const Panel = styled.div`
